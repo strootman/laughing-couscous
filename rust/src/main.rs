c7d0dc57bdc5f7c71a11922f056fb24ac96379dd
Jonathan Strootman <strootman@gmail.com>
2025-01-29 16:52:08 -0700

    Extract `find_matches` function for testing
    Add some notes, including the src tutorial, and other crates for future reference

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,26 +1,41 @@
 // use ferris_says::say;
 // use std::io::{stdout, BufWriter};
+// use indicatif::ProgressBar;
 use anyhow::{Context, Result};
 use clap::Parser;
 
-// Search for a pattern in a file and display the lines that contain it
+/* Notes: 
+ * - Trying to put the `#[derive(Debug)]` macro on this type is interesting due to the `PathBuf`.
+ *   I punted on this, because `PathBuf` does not implement the `Display` trait. You get into 
+ *   ownership issues pretty quickly. Seems solvable, I just decided to move on.
+ */
 #[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
 
+fn find_matches(content: &str, pattern: &str) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            println!("{}", line);
+        }
+    }
+}
+
+// Search for a pattern in a file and display the lines that contain it
+/*
+ * https://rust-cli.github.io/book/index.html 
+ * 
+ */
 fn main() -> Result<()> {
+    // let pb = indicatif::ProgressBar::new(100);
     let args = Cli::parse();
     let path = &args.path.into_os_string().into_string().unwrap();
     let content =
         std::fs::read_to_string(path).with_context(|| format!("Could not read file `{}`", path))?;
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    find_matches(&content, &args.pattern);
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, path);
+    println!("pattern: {}, path: {:?}", args.pattern, path);
     Ok(())
 }
