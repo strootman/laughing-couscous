c5f4656b51a7b976ec9af9ab126ce4d35525da15
Jonathan Strootman <strootman@gmail.com>
2025-01-30 14:05:36 -0700

    Add `find_content_in_file` integration test

--- rust/tests/cli.rs
+++ rust/tests/cli.rs
@@ -1,4 +1,5 @@
 use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
 use predicates::prelude::*;
 use std::process::Command;
 
@@ -12,4 +13,19 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
         .failure()
         .stderr(predicate::str::contains("Could not read file"));
     Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("hello-rust")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("A test\nAnother test"));
+
+    Ok(())
+
 }
